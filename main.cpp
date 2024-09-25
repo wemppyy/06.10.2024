e18910d451e9bfc289498f810a18bfa1f94bd86f
wemppyy <denisfrostaliuk@gmail.com>
2024-09-25 20:43:17 +0300

    done

--- main.cpp
+++ main.cpp
@@ -13,14 +13,6 @@ T* create_arr(int size) {
 }
 
 template <typename T>
-void array_sum(T* arr1, T* arr2, T* arr3, int size) {
-	for (size_t i = 0; i < size; i++)
-	{
-		arr3[i] = arr1[i] + arr2[i];
-	}
-}
-
-template <typename T>
 void print_arr(T* arr, int size) {
 	for (size_t i = 0; i < size; i++)
 	{
@@ -28,24 +20,71 @@ void print_arr(T* arr, int size) {
 	}cout << endl;
 }
 
+template <typename T>
+T get_max(T* arr1, T* arr2, int size1, int size2) {
+	int max1 = arr1[0], max2 = arr2[0];
+	for (int i = 0; i < size1; i++) {
+		if (arr1[i] > max1) max1 = arr1[i];
+	}
+	for (int i = 0; i < size2; i++) {
+		if (arr2[i] > max2) max2 = arr1[i];
+	}
+	return max1 > max2 ? max1 : max2;
+}
+
+template <typename T>
+T get_min(T* arr1, T* arr2, int size1, int size2) {
+	int min1 = arr1[0], min2 = arr2[0];
+	for (int i = 0; i < size1; i++) {
+		if (arr1[i] < min1) min1 = arr1[i];
+	}
+	for (int i = 0; i < size2; i++) {
+		if (arr2[i] < min2) min2 = arr1[i];
+	}
+	return min1 < min2 ? min1 : min2;
+}
+
+template <typename T>
+T get_avg(T* arr1, T* arr2, int size1, int size2) {
+	int avg1 = 0, avg2 = 0;
+	for (size_t i = 0; i < size1; i++)
+	{
+		avg1 += arr1[i];
+	}for (size_t i = 0; i < size2; i++)
+	{
+		avg2 += arr2[i];
+	}
+	avg1 /= size1;
+	avg2 /= size2;
+	return (avg1 + avg2) / 2;
+}
+
+template <typename T>
+void get_action(T* arr1, T* arr2, int size1, int size2, int(*ftpr[3])(int*, int*, int, int)) {
+	int operation;
+	cout << "Choose operation\n"
+			"1. Get max\n"
+			"2. Get min\n"
+			"3. Get avarage\n";
+	cin >> operation;
+	cout << ftpr[operation-1](arr1, arr2, size1, size2);
+}
+
+
 int main() {
 	srand(time(0));
-	int arr_size;
-	cout << "Enter size of arrys: ";
-	cin >> arr_size;
-	int* arr1 = create_arr<int>(arr_size);
-	int* arr2 = create_arr<int>(arr_size);
-	int* arr3 = new int[arr_size]();
-	array_sum(arr1, arr2, arr3, arr_size);
-	cout << "1. arr = ";
-	print_arr(arr1, arr_size);
-	cout << "2. arr = ";
-	print_arr(arr2, arr_size);
-	cout << "result = ";
-	print_arr(arr3, arr_size);
-
-
-
+	int(*ftpr[3])(int*, int*, int, int) = {get_max, get_min, get_avg};
+	int arr1_size, arr2_size;
+	cout << "Enter size of first array: ";
+	cin >> arr1_size;
+	int* arr1 = create_arr<int>(arr1_size);
+	print_arr(arr1, arr1_size);
+	cout << "Enter size of second array: ";
+	cin >> arr2_size;
+	int* arr2 = create_arr<int>(arr2_size);
+	print_arr(arr2, arr2_size);
+	cout << endl;
+	get_action(arr1, arr2, arr1_size, arr2_size, ftpr);
 
 
 	return 0;
